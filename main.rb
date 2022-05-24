d40734facffa0630fa4be8a8e607552d6f19f892
Henry Doan <henrydoan96@gmail.com>
2022-05-23 20:34:17 -0600

    Update main.rb

--- main.rb
+++ main.rb
@@ -6,12 +6,12 @@ def puts_git(cmd)
 end
 
 def menu
-  puts '1: Enter git command'.colorize(:green)
+  puts '1: Enter git command'.colorize(:black)
   puts '2: Exit'.colorize(:red)
   choice = gets.to_i
   case choice
   when 1
-    puts 'Enter git command'.colorize(:blue)
+    puts 'Enter git command'.colorize(:green)
     puts_git(gets.strip)
     menu
   when 2
